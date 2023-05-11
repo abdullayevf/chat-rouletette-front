a8e8c65cba9f4d4d627ae892072ae4052d1e1264
dasturchioka <dasturchioka@gmail.com>
2023-05-11 22:14:07 +0500

    axios pls

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -5,7 +5,7 @@ export const auth = axios.create({
   baseURL: "https://api.chat-roulet.ru/api/auth",
 });
 
-export const global = axios.create({
+export const universal = axios.create({
   baseURL: "http://api.chat-roulet.ru/api/",
   headers: {
     Authorization: Cookies.get("accessToken"),
